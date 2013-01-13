70b4837f3dfc56a9ef1a3858cb7ee402a32da92a
ddgriffin <dianadgriffin@gmail.com>
2013-01-13 15:23:19 -0600

    create railsgirls app, add carrierwave uploader, add imagemagick for thumbnails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '071a44172dd986e0180565c1367ae39882f95fcfe86f75d4a79523e33e98390166ee6e030582a83296f51e0ffed423dd9ee5353cc23068fc5d56d2082651f150'
